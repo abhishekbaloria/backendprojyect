d1e13a5e9b8494f388ea83bdc3bdbe99c05c53fa
abhishekbaloria <abhia3477@gmail.com>
2025-06-26 23:41:54 +0200

    prodcut loading and main tab layout

--- backend.py
+++ backend.py
@@ -14,3 +14,19 @@ class KidsClothingApp:
         self.window.title("TinyThreads Store")
         self.window.geometry("800x600")
         self.window.configure(bg="#e3f2fd")
+
+        # thss will help load products from a JSON file
+        self.products = self.load_product_list()
+        self.shopping_cart = []
+
+        # this area is for the main tab layout
+        self.tabs = ttk.Notebook(window)
+        self.tabs.pack(fill="both", expand=True, padx=10, pady=10)
+
+        self.page_home = ttk.Frame(self.tabs)
+        self.page_cart = ttk.Frame(self.tabs)
+        self.page_checkout = ttk.Frame(self.tabs)
+
+        self.tabs.add(self.page_home, text="Shop")
+        self.tabs.add(self.page_cart, text="My Cart")
+        self.tabs.add(self.page_checkout, text="Buy Now")
